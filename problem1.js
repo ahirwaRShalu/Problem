e4194d31ab9c76cf2c5bd24e20f0e8f5dfdd621c
unknown <shaluahirwar016@gmail.com>
2022-12-01 20:07:04 +0530

    problem_1

---
+++ problem1.js
@@ -0,0 +1,11 @@
+Check whether a number is Prime or no
+
+let num=13;
+let count=0;
+for(let i=2 ; i<num ;i++)
+{
+if(num%i==0)
+{
+count++;
+}
+}
---
+++ problem2.js
@@ -0,0 +1,8 @@
+Check whether a string is palindrome or not.
+
+let str="madam";
+let new_str="";
+for(let i=str.length-1 ; i>=0 ;i--)
+{
+new_str=new_str+str[i];
+}
